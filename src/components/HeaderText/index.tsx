72c0c31b3689170b3d50b09764651618f1d81821
SinaNasibparast <dev.sinanp@gmail.com>
2025-09-17 00:52:55 +0300

    Update index.tsx

--- src/components/HeaderText/index.tsx
+++ src/components/HeaderText/index.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function HeaderText() {
   return (
-    <div className='text-center w-full'>
-      <h2 className='text-[52px] font-bold'>
+    <div className='text-center w-full max-lg:w-1/2 max-sm:w-[80%]'>
+      <h2 className='text-[52px] max-lg:text-[48px] max-sm:text-[40px] font-bold'>
         How’s the sky looking today?
       </h2>
     </div>
