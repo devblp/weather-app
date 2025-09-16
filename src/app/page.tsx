34cc2dbec0b500582d26a55a861ed13798877749
SinaNasibparast <dev.sinanp@gmail.com>
2025-09-17 00:52:35 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import HeaderText from "@/components/HeaderText";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-16 justify-center items-center px-28 w-full">
+    <div className="flex flex-col gap-16 justify-center items-center px-28 max-lg:px-7 w-full">
       {/* Header */}
       <Header />
       {/* Header Text */}
