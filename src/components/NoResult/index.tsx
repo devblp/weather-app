0e640b5a9703d89e8feb92f970a4671090ece9c9
SinaNasibparast <dev.sinanp@gmail.com>
2025-09-17 00:52:57 +0300

    Create index.tsx

---
+++ src/components/NoResult/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NoResult() {
+  return (
+    <div className='flex justify-center items-center h-52'>
+        <p className='text-2xl font-bold'>No search result found !</p>
+    </div>
+  )
+}
