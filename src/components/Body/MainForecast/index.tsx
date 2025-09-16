56afab89b46d68eb83966fbd3377ec7995891c3e
SinaNasibparast <dev.sinanp@gmail.com>
2025-09-17 00:52:49 +0300

    Update index.tsx

--- src/components/Body/MainForecast/index.tsx
+++ src/components/Body/MainForecast/index.tsx
@@ -16,12 +16,12 @@ export default function MainForecast() {
     })()
   }, [])
   return (
-    <div className='flex justify-between items-center bg-[#4658D9] rounded-[20px] w-full h-72 p-6'>
-      <div className='flex flex-col gap-2'>
+    <div className='flex max-sm:flex-col justify-between items-center bg-[#4658D9] rounded-[20px] w-full h-72 p-6'>
+      <div className='flex max-sm:items-center flex-col gap-2'>
         <p className='text-[28px] font-bold'>{IPdata?.city}, {IPdata?.country_name}</p>
         <p className='text-[18px]'>Tuesday, Aug 5, 2025</p>
       </div>
-      <div className='flex items-center gap-5'>
+      <div className='flex max-sm:items-center items-center gap-5'>
         <img src="/Sunny.png" alt="" />
         <p className='text-8xl '>20°</p>
       </div>
