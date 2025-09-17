7d7bd98482376d93ac4798f5b60dcfa000d0df4d
SinaNasibparast <dev.sinanp@gmail.com>
2025-09-18 01:58:02 +0300

    add new page

---
+++ src/components/APIerror/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import BtnRetry from '../ui/Buttons/BtnRetry'
+
+export default function APIerror() {
+    return (
+        <div className='flex flex-col justify-center items-center gap-5'>
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#ffffff" viewBox="0 0 256 256">
+                    <path d="M128,20A108,108,0,1,0,236,128,108.12,108.12,0,0,0,128,20Zm84,108a83.6,83.6,0,0,1-16.75,50.28L77.72,60.75A84,84,0,0,1,212,128ZM44,128A83.6,83.6,0,0,1,60.75,77.72L178.28,195.25A84,84,0,0,1,44,128Z"></path>
+                </svg>
+            </div>
+            <div>
+                <p className='text-[52px] max-sm:text-[42px] text-center'>Something went wrong</p>
+            </div>
+            <div className='text-center w-[60%] max-sm:w-full'>
+                <p className='text-[#D4D3D9] text-[20px]'>We couldn’t connect to the server (API error). Please try again in a few moments.</p>
+            </div>
+            <div>
+                <BtnRetry />
+            </div>
+        </div>
+    )
+}
--- src/components/Body/index.tsx
+++ src/components/Body/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import SearchBar from './SearchBar'
 import Layout from './Layout'
-import NoResult from '../NoResult'
+// import NoResult from '../NoResult'
 
 export default function Body() {
   return (
     <div className='w-full'>
       <SearchBar />
-      <NoResult/>
+      <Layout/>
     </div>
   )
 }
