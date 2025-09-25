cdc27f633d8aeab9c3ca17224025c073fbf7957a
SinaNasibparast <dev.sinanp@gmail.com>
2025-09-25 22:45:36 +0300

    Update index.tsx

--- src/components/Body/Layout/index.tsx
+++ src/components/Body/Layout/index.tsx
@@ -3,61 +3,78 @@ import MainForecast from '../MainForecast'
 import HourlyForecast from '../HourlyForecast'
 import DiethylForecast from '../DiethylForecast'
 import DailyForecast from '../DailyForecast'
-
+import SearchBar from '../SearchBar'
+// import NoResult from '@/components/NoResult'
 interface IPdata {
-    city: string;
-    country: string;
-    lat: string;
-    lon: string
+  city: string;
+  country: string;
+  lat: string;
+  lon: string
 }
 
 interface Data {
-    current: {
-        apparent_temperature: string;
-        precipitation: number;
-        relative_humidity_2m: number;
-        wind_speed_10m: number;
-        temperature_2m: number;
-        time: string;
-        weather_code: number;
-        is_day: number;
-    };
-    daily: {
-        temperature_2m_max: string[];
-        temperature_2m_min: string[];
-        time: string[];
-        weather_code: string[];
-    };
-    hourly: {
-        temperature_2m: string[];
-        time: string[];
-        weather_code: string[];
-    }
+  current: {
+    apparent_temperature: string;
+    precipitation: number;
+    relative_humidity_2m: number;
+    wind_speed_10m: number;
+    temperature_2m: number;
+    time: string;
+    weather_code: number;
+    is_day: number;
+  };
+  daily: {
+    temperature_2m_max: string[];
+    temperature_2m_min: string[];
+    time: string[];
+    weather_code: string[];
+  };
+  hourly: {
+    temperature_2m: string[];
+    time: string[];
+    weather_code: string[];
+  }
 }
 
 interface Units {
-    temperature: {
-        c: boolean;
-        f: boolean;
-    };
-    windSpeed: {
-        kmh: boolean;
-        mph: boolean;
-    };
-    precipitation: {
-        mm: boolean;
-        in: boolean;
-    }
+  temperature: {
+    c: boolean;
+    f: boolean;
+  };
+  windSpeed: {
+    kmh: boolean;
+    mph: boolean;
+  };
+  precipitation: {
+    mm: boolean;
+    in: boolean;
+  }
 }
+
+interface SearchData {
+  lat: string;
+  lon: string;
+  name: string;
+  display_name: string;
+}
+
+interface CityData {
+  city: string;
+  land: string;
+}
+
 interface Props {
-    units: Units
+  units: Units
+  setServer: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-
-export default function Layout({ units }: Props) {
+export default function Layout({ units, setServer }: Props) {
   const [IPdata, setIPdata] = useState<IPdata>()
   const [data, setData] = useState<Data>()
   const [loading, setLoading] = useState(true)
+  const [search, setSearch] = useState("")
+  const [items, setItesm] = useState<SearchData[]>([])
+  const [cityData, setCityData] = useState<CityData | undefined>()
 
   useEffect(() => {
     (async () => {
@@ -75,36 +92,49 @@ export default function Layout({ units }: Props) {
   const handleWeather = async (latitude: string, longitude: string) => {
     setLoading(true)
     const res = await fetch(
-      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=is_day,weather_code,apparent_temperature,is_day,relative_humidity_2m,wind_speed_10m,precipitation,rain,temperature_2m&daily=temperature_2m_max,temperature_2m_min,weather_code&hourly=weather_code,temperature_2m${
-        units.windSpeed.mph ? "&wind_speed_unit=mph" : ""
-      }${units.temperature.f ? "&temperature_unit=fahrenheit" : ""}${
-        units.precipitation.in ? "&precipitation_unit=inch" : ""
+      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=is_day,weather_code,apparent_temperature,is_day,relative_humidity_2m,wind_speed_10m,precipitation,rain,temperature_2m&daily=temperature_2m_max,temperature_2m_min,weather_code&hourly=weather_code,temperature_2m${units.windSpeed.mph ? "&wind_speed_unit=mph" : ""
+      }${units.temperature.f ? "&temperature_unit=fahrenheit" : ""}${units.precipitation.in ? "&precipitation_unit=inch" : ""
       }`
     )
     const json = await res.json()
+    if (!res.ok) {
+      setServer(false)
+    }
     setData(json)
     setLoading(false)
-    console.log(json)
+    if (items[0]) {
+      setCityData({ city: items[0].name, land: items[0]?.display_name.split(",").pop() || "" })
+    }
   }
 
-//   if(loading)return<div>...loading</div>
+  // if(loading)return<NoResult/>
 
   return (
-    <div className="flex max-lg:flex-col gap-10 w-full pb-20">
-      <div className="w-[65%] max-lg:w-full">
-        <MainForecast data={data} IPdata={IPdata} />
-        <DiethylForecast
-          precipitation={data?.current?.precipitation}
-          relative_humidity_2m={data?.current?.relative_humidity_2m}
-          wind_speed_10m={data?.current?.wind_speed_10m}
-          temperature_2m={data?.current?.temperature_2m}
-          units={units}
-        />
-        <DailyForecast data={data} />
-      </div>
-      <div className="w-[35%] max-lg:w-full">
-        <HourlyForecast data={data} />
+    <div className='w-full'>
+      <SearchBar searche={search}
+        setSearche={setSearch}
+        items={items}
+        setItesm={setItesm}
+        handleWeather={handleWeather}
+      />
+
+      <div className="flex max-lg:flex-col gap-10 w-full pb-20">
+        <div className="w-[65%] max-lg:w-full">
+          <MainForecast data={data} IPdata={IPdata} loading={loading} cityData={cityData} />
+          <DiethylForecast
+            precipitation={data?.current?.precipitation}
+            relative_humidity_2m={data?.current?.relative_humidity_2m}
+            wind_speed_10m={data?.current?.wind_speed_10m}
+            temperature_2m={data?.current?.temperature_2m}
+            units={units}
+          />
+          <DailyForecast data={data} />
+        </div>
+        <div className="w-[35%] max-lg:w-full">
+          <HourlyForecast data={data} />
+        </div>
       </div>
     </div>
+
   )
 }
