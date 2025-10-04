1bfefd63d215e13b5759aaef52eb076a46a132e9
devblp <dev.sinanp@gmail.com>
2025-10-05 02:40:09 +0300

    fix golobals.cdd

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
-const repoName = "/weather-app"; 
-
 const nextConfig: NextConfig = {
-  basePath: repoName,
-  assetPrefix: repoName,
-  reactStrictMode: true,
-  output: "export", 
+  /* config options here */
 };
 
 export default nextConfig;
